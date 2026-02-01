5e4a6a91cb411f3e59f594f7cb30447d682f5ddf
youzai8913 <yanxiao8913@163.com>
2026-02-01 16:03:44 +0800

    feat:support win

--- skill_hub/__main__.py
+++ skill_hub/__main__.py
@@ -1,3 +1,11 @@
+import sys
+
+# 设置 Windows 控制台编码为 UTF-8
+if sys.platform == 'win32':
+    import io
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 from .__init__ import main
 
 if __name__ == '__main__':
